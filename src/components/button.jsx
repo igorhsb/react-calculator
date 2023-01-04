ee881d71cba0a8e0041a6108536b641e662cb13a
igorhsb <igorhsb37@gmail.com>
2023-01-04 16:02:42 -0300

    done

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React from 'react';
 import './button.css'
 
 let mapID = {
-    0 : {id : "zero", label: "0"},
-    1 : {id : "one", label: "1"},
-    2 : {id : "two", label: "2"},
-    3 : {id : "three", label: "3"},
-    4 : {id : "four", label: "4"},
-    5 : {id : "five", label: "5"},
-    6 : {id : "six", label: "6"},
-    7 : {id : "seven", label: "7"},
-    8 : {id : "eight", label: "8"},
-    9 : {id : "nine", label: "9"},
-    10 : {id : "plus", label: "+"},
-    11 : {id : "minus", label: "-"},
-    12 : {id : "mult", label: "x"},
-    13 : {id : "div", label: "/"},
-    14 : {id : "mod", label: "%"},
-    15 : {id : "equal", label: "="},
-    16 : {id : "point", label: "."},
-    17 : {id : "ac", label: "Clear"},
+    0 : {id : 'zero', label: '0'},
+    1 : {id : 'one', label: '1'},
+    2 : {id : 'two', label: '2'},
+    3 : {id : 'three', label: '3'},
+    4 : {id : 'four', label: '4'},
+    5 : {id : 'five', label: '5'},
+    6 : {id : 'six', label: '6'},
+    7 : {id : 'seven', label: '7'},
+    8 : {id : 'eight', label: '8'},
+    9 : {id : 'nine', label: '9'},
+    10 : {id : 'plus', label: '+'},
+    11 : {id : 'minus', label: '-'},
+    12 : {id : 'mult', label: 'x'},
+    13 : {id : 'div', label: '/'},
+    14 : {id : 'mod', label: '%'},
+    15 : {id : 'equal', label: '='},
+    16 : {id : 'point', label: '.'},
+    17 : {id : 'ac', label: 'Clear'},
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
-        <button className="button" id={mapID[props.id].id}>{mapID[props.id].label}</button>
+        <button 
+            className='button' 
+            id={mapID[props.id].id} 
+            onClick={e => props.click && props.click(mapID[props.id].label)}
+            >{mapID[props.id].label}</button>
    
     
--- src/main/calculator/Calculator.jsx
+++ src/main/calculator/Calculator.jsx
@@ -3,29 +3,105 @@ import './Calculator.css'
 import Button from "../../components/button.jsx"
 import Display from "../../components/display.jsx"
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component{
+    state = {...initialState}
+
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
+    clearMemory() {
+        this.setState({...initialState})
+    }
+
+    setOperation(op) {
+        if(this.state.current === 0) {
+            this.setState({operation:op,  current:1, clearDisplay:true})
+        } else {
+            const equals = op === '='
+            const currentOperation = this.state.operation
+            let result = 0;
+            let values = [...this.state.values]
+            // eslint-disable-next-line default-case
+            switch(currentOperation) {
+                case 'x':
+                    result = values[0] * values[1]
+                    break
+                case '-':
+                    result = values[0] - values[1]
+                    break
+                case '+':
+                    result = values[0] + values[1]
+                    break
+                case '/':
+                    result = values[0] / values[1]
+                    break
+                case '%':
+                    result = values[0] % values[1]
+                    break
+                case '=':
+                    result = values[0]
+                    break
+            }
+            values = [result, 0]
+            this.setState({values,  current:0, displayValue:result.toString()})
+            console.log('eq', result)
+            console.log('cur', currentOperation)
+            console.log('values', values)
+        }
+    }
+
+    addDigit(d) {
+        if(d === "." && this.state.displayValue.includes('.')) {
+            return
+        }
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + d
+        this.setState({displayValue, clearDisplay: false})
+        if(d !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({values })
+        }
+        console.log(this.state.values)
+    }
+
     render() {
         return (
             <div className="calculator">
-                <Display value={100}/>
-                <Button id="0"/>
-                <Button id="1"/>
-                <Button id="2"/>
-                <Button id="3" />
-                <Button id="4"/>
-                <Button id="5"/>
-                <Button id="6"/>
-                <Button id="7"/>
-                <Button id="8"/>
-                <Button id="9"/>
-                <Button id="10"/>
-                <Button id="11"/>
-                <Button id="12"/>
-                <Button id="13"/>
-                <Button id="14"/>
-                <Button id="15"/>
-                <Button id="16"/>
-                <Button id="17"/>
+                <Display value={this.state.displayValue}/>
+                <Button id="0" click={this.addDigit}/>
+                <Button id="1" click={this.addDigit}/>
+                <Button id="2" click={this.addDigit}/>
+                <Button id="3" click={this.addDigit}/>
+                <Button id="4" click={this.addDigit}/>
+                <Button id="5" click={this.addDigit}/>
+                <Button id="6" click={this.addDigit}/>
+                <Button id="7" click={this.addDigit}/>
+                <Button id="8" click={this.addDigit}/>
+                <Button id="9" click={this.addDigit}/>
+                <Button id="10" click={this.setOperation}/>
+                <Button id="11" click={this.setOperation}/>
+                <Button id="12" click={this.setOperation}/>
+                <Button id="13" click={this.setOperation}/>
+                <Button id="14" click={this.setOperation}/>
+                <Button id="15" click={this.setOperation}/>
+                <Button id="16" click={this.addDigit}/>
+                <Button id="17" click={this.clearMemory}/>
             </div>
         )
     }
